b50f0141795ad7d68d6007d3e55be378947fb20c
Artem <rav051098@gmail.com>
2020-11-13 16:58:56 +0300

    dz 4atik

---
+++ Java2_7/src/server/Server.java
@@ -0,0 +1,15 @@
+package server;
+
+import auth.AuthenticationService;
+
+import java.util.Set;
+
+public interface Server {
+    void broadcastMessage(String message);
+    void sendPrivateMessage(String from, String name, String privateMessage);
+    boolean isLoggedIn(String nickname);
+    void subscribe(ClientHandler client);
+    void unsubscribe(ClientHandler client);
+    AuthenticationService getAuthenticationService();
+    Set<ClientHandler> getClients();
+}
